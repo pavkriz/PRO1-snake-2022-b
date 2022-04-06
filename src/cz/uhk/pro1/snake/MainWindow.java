3ecc9a4b02c1bab77788c40b75eadd5d3b00eb99
Pavel Kriz <pavel.kriz@uhk.cz>
2022-04-06 18:22:30 +0200

    Tick timer

--- src/cz/uhk/pro1/snake/MainWindow.java
+++ src/cz/uhk/pro1/snake/MainWindow.java
@@ -4,17 +4,33 @@ import cz.uhk.pro1.snake.model.GameBoard;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Date;
 
 public class MainWindow extends JFrame {
-    private GameBoard gameBoard = new GameBoard();
+    private final Timer timer = new Timer(500, e -> tick());    // casovac, ktery spousti metodu tick kazdych 500ms
+    private final GameBoard gameBoard = new GameBoard(); // presunuto z kostruktoru sem, mj. aby byla promenna gameBoard viditelna v tick()
+    private final CanvasPanel p = new CanvasPanel(gameBoard);
 
     public MainWindow() {
         setTitle("Snake");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        CanvasPanel p = new CanvasPanel(gameBoard);
+        p.setDoubleBuffered(true); // zapneme potencialne plynulejsi animace
         add(p);
         p.setPreferredSize(new Dimension(300, 300));
         pack();
+        timer.start();  // zapneme casovac
+    }
+
+    public void tick() {
+        System.out.println(new Date()+" tick!");
+        // TODO volitelne (extra bod): zkontrolovat, zda hrac pohybem vpred nevyjede z herni plochy ( jeji rozmer si muzete si definovat pevne napr. 20x20)
+        // TODO povinne: posunout hada o 1 krok
+
+        // vyvolame prekresleni herni plochy na JPanelu (CanvasPanelu):
+        p.repaint();
+        // kvuli lagovani v Linuxu: Synchronizes this toolkit's graphics state. Some window systems may do buffering
+        // of graphics events. This method ensures that the display is up-to-date. It is useful for animation.
+        Toolkit.getDefaultToolkit().sync();
     }
 
     public static void main(String[] args) {
